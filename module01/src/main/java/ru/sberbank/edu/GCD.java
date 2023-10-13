cab51a029ac2c581afa142474c02d842c2077b5b
Dmitry <karpov0910@mail.ru>
2023-10-14 00:54:42 +0300

    добавлен класс GreetingImpl и GCD

---
+++ module01/src/main/java/ru/sberbank/edu/GCD.java
@@ -0,0 +1,8 @@
+package ru.sberbank.edu;
+
+public class GCD implements CommonDivisor{
+    @Override
+    public int getDivisor(int firstNumber, int secondNumber) {
+        return 0;
+    }
+}
---
+++ module01/src/main/java/ru/sberbank/edu/GreetingImpl.java
@@ -0,0 +1,8 @@
+package ru.sberbank.edu;
+
+public class GreetingImpl implements Greeting{
+    @Override
+    public String getBestHobby() {
+        return null;
+    }
+}
