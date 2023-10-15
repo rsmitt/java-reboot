bc8e21039171168cfda0dc01aac0e9f1ce464b31
Dmitry <karpov0910@mail.ru>
2023-10-15 19:05:17 +0300

    На проверку

--- module01/src/main/java/ru/sberbank/edu/GreetingImpl.java
+++ module01/src/main/java/ru/sberbank/edu/GreetingImpl.java
@@ -1,8 +1,19 @@
 package ru.sberbank.edu;
 
 public class GreetingImpl implements Greeting{
+    /**
+     * Метод выводит информацию о разработчике данного класса
+     */
     @Override
     public String getBestHobby() {
-        return null;
+        String name = "Дмитрий";
+        String lastName = "Карпов";
+        String post = "Инженер по нагрузочному тестированию";
+        String hobby = "Увлекаюсь програмированием на Java, люблю активные виды спорта \n" +
+                "(волейбол, настольный тенис, катание на велосипеде) и путешествия";
+        return "Имя: " + name + ",\n" +
+                "Фамилия: " + lastName + ",\n" +
+                "Должность: " + post + ",\n" +
+                "Хобби: " + hobby;
     }
 }
