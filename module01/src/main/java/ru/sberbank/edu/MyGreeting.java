934973b644bf3fe847347716daaf0e3f48dfaec1
Alex <alexassaiche@gmail.com>
2023-10-17 00:08:29 +0300

    module 1 solution

---
+++ module01/src/main/java/ru/sberbank/edu/MyGreeting.java
@@ -0,0 +1,13 @@
+package ru.sberbank.edu;
+/**
+ * @author      Чернов Алексей
+ */
+public class MyGreeting implements Greeting{
+    /**
+     * Метод возвращает строку с информацией о хобби
+     */
+   public String getBestHobby(){
+      return "My hobby is long-distance running";
+    }
+
+}
