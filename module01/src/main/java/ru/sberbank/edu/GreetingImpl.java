e1fd000bab30f49f408c19a2cebaff3937dbad7c
Newarin <Newarin@gmail.com>
2023-10-17 20:35:23 +0300

    Добавил ентеров для пул реквеста

--- module01/src/main/java/ru/sberbank/edu/GreetingImpl.java
+++ module01/src/main/java/ru/sberbank/edu/GreetingImpl.java
@@ -5,11 +5,15 @@ public class GreetingImpl implements Greeting {
      * В данном методе мы всегда выводим как лучшее хобби Водный туризм
      */
     public void main(String[] args) {
+
         System.out.println(getBestHobby());
+
     }
 
     @Override
     public String getBestHobby() {
+
         return "Водный туризм";
+
     }
 }
