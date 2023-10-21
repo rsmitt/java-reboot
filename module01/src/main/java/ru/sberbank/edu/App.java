9509afde81ad08293cf007e6e47b526858838fb3
kays <kaisnof@gmail.com>
2023-10-21 17:19:37 +0300

    1 задание по Java

--- module01/src/main/java/ru/sberbank/edu/App.java
+++ module01/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,13 @@
 package ru.sberbank.edu;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        GreetingImpl greeting = new GreetingImpl();
+        System.out.println(greeting.getBestHobby());
+        GCD gcd = new GCD();
+        System.out.println(gcd.getDivisor(0, 0));
+        System.out.println(gcd.getDivisor(-100, 20));
+        System.out.println(gcd.getDivisor(7, 29));
     }
 }
