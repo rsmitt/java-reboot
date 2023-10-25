1578b716aee087098df8c1cd628959c7206b35a7
Dmitry <karpov0910@mail.ru>
2023-10-25 06:24:33 +0300

    Сохранение в бд переделано через интерфейс

--- module02/src/main/java/ru/sberbank/edu/App.java
+++ module02/src/main/java/ru/sberbank/edu/App.java
@@ -4,10 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args ) throws IOException {
