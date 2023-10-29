20d0f89943027535f92ade0a9e9b63c28d899ce2
kays <kaisnof@gmail.com>
2023-10-29 21:51:13 +0300

    4 дз по Java

--- module04/src/main/java/ru/sberbank/edu/App.java
+++ module04/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,16 @@
 package ru.sberbank.edu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+//        CustomDigitComparator comparator = new CustomDigitComparator();
+//        System.out.println(comparator.compare(1, 2));
+//        System.out.println(comparator.compare(2, 1));
+//        System.out.println(comparator.compare(1, 1));
+//        System.out.println(comparator.compare(2, 2));
+//        System.out.println(comparator.compare(2, null));
+
+        Person person1 = new Person("Kays", "Moscow", 30);
+        Person person2 = new Person("Akhmad", "Tver", 54);
+        System.out.println(person1.compareTo(person2));
     }
 }
