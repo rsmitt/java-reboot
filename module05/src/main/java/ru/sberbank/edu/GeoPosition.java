bdc16f51e26cd315274b2c6f2b4b32a1d3ee712f
Karpov-D <karpov0910@mail.ru>
2023-11-09 07:36:50 +0300

    начало дз_5

--- module05/src/main/java/ru/sberbank/edu/GeoPosition.java
+++ module05/src/main/java/ru/sberbank/edu/GeoPosition.java
@@ -23,8 +23,50 @@ public class GeoPosition {
      *                        Possible values: 55, 55(45'07''), 59(57'00'')
      */
     public GeoPosition(String latitudeGradus, String longitudeGradus) {
-        // parse and set latitude and longitude in radian
+        if (latitudeGradus.length() <= 3) {
+            latitude = Math.toRadians(Double.parseDouble(latitudeGradus));
+        }
+        else if (longitudeGradus.length() <= 3) {
+            longitude = Math.toRadians(Double.parseDouble(longitudeGradus));
+        }
+        else if (longitudeGradus.length() > 3) {
+            double grad = 0.0;
+            double minutes = 0.0;
+            double sec = 0.0;
+            latitude = grad + (minutes / 60.0) + (sec / 3600.0);
+        }
+        else if (longitudeGradus.length() > 3) {
+
+        }
+        else {
+            System.out.println("Введите значения в формате 55 или 55(45'07'')");
+        }
+
+        latitude = Math.toRadians(Double.parseDouble(latitudeGradus));
+        longitude = Math.toRadians(Double.parseDouble(longitudeGradus));
     }
 
-    // getters and toString
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "GeoPosition{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
 }
