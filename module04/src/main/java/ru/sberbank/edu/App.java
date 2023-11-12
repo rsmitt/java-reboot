4ca560c4de812259537de7fe82c119897aee109e
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2023-11-12 21:09:34 +0300

    Реализовал класс Person.

--- module04/src/main/java/ru/sberbank/edu/App.java
+++ module04/src/main/java/ru/sberbank/edu/App.java
@@ -8,6 +8,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /*Person person1 = new Person("Oleg","Moscow",15);
+        Person person2 = new Person("Egor","Kasimov",16);
+        Person person3 = new Person("Egor","KaSImov",16);
+
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
+
+        System.out.println(person2.equals(person1));
+        System.out.println(person2.equals(person3));
+
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+        System.out.println(person3.hashCode());
+         */
+
     }
 }
