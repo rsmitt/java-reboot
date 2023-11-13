54214a606f9c48ecca6864a27e3ca2e745d8e54c
bogdanzernovoj <jernowoi.bogdan@yandex.ru>
2023-11-13 18:56:02 +0300

    Реализован класс CustomDigitComparator

--- module04/src/main/java/ru/sberbank/edu/App.java
+++ module04/src/main/java/ru/sberbank/edu/App.java
@@ -1,5 +1,9 @@
 package ru.sberbank.edu;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
+        int i1 = 1, i2 = 2, i3 = 3, i4 = 4;
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
+        Comparator<Integer> comp = new CustomDigitComparator();
+        list.sort(comp);
+        System.out.println(list);
         /*Person person1 = new Person("Oleg","Moscow",15);
         Person person2 = new Person("Egor","Kasimov",16);
         Person person3 = new Person("Egor","KaSImov",16);
