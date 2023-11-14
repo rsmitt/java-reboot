88b28568dc568b90ec1284e7e93ec65d68792f69
Dmitry <karpov0910@mail.ru>
2023-11-14 08:17:06 +0300

    ДЗ5 добавлен метод getDistance

--- module05/src/main/java/ru/sberbank/edu/TravelService.java
+++ module05/src/main/java/ru/sberbank/edu/TravelService.java
@@ -2,6 +2,8 @@ package ru.sberbank.edu;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Travel Service.
@@ -9,7 +11,7 @@ import java.util.List;
 public class TravelService {
 
     // do not change type
-    private final List<CityInfo> cities = new ArrayList<>();
+    private List<CityInfo> cities = new ArrayList<>();
 
     /**
      * Append city info.
@@ -28,6 +30,9 @@ public class TravelService {
      * @throws IllegalArgumentException if city doesn't exist
      */
     public void remove(String cityName) {
+        Stream<CityInfo> streamOfCities = cities.stream();
+        cities = streamOfCities.filter(x -> !cityName.equals(x.getName()))
+                .collect(Collectors.toList());
 
     }
 
@@ -35,7 +40,9 @@ public class TravelService {
      * Get cities names.
      */
     public List<String> citiesNames() {
-        return null;
+        Stream<CityInfo> streamOfCities = cities.stream();
+        return streamOfCities.map(CityInfo::getName)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -46,8 +53,23 @@ public class TravelService {
      * @param destCityName - destination city
      * @throws IllegalArgumentException if source or destination city doesn't exist.
      */
-    public int getDistance(String srcCityName, String destCityName) {
-        return 0;
+    public double getDistance(String srcCityName, String destCityName) {
+        Stream<CityInfo> streamOfCities = cities.stream();
+        List<CityInfo> citiesResult = streamOfCities.filter(x -> srcCityName.equals(x.getName()) ||
+                destCityName.equals(x.getName())).toList();
+        GeoPosition geo_1 = citiesResult.get(0).getPosition();
+        GeoPosition geo_2 = citiesResult.get(1).getPosition();
+        double cl1 = Math.cos(geo_1.getLatitude());
+        double cl2 = Math.cos(geo_2.getLatitude());
+        double sl1 = Math.sin(geo_1.getLatitude());
+        double sl2 = Math.sin(geo_2.getLatitude());
+        double delta = geo_1.getLongitude() - geo_2.getLongitude();
+        double cDelta = Math.cos(delta);
+        double sDelta = Math.sin(delta);
+        double y = Math.sqrt(Math.pow(cl2 * sDelta, 2) + Math.pow(cl1 * sl2 - sl1 * cl2 * cDelta, 2));
+        double x = sl1 * sl2 + cl1 * cl2 * cDelta;
+        double ad = Math.atan2(y, x);
+        return ad * 6372795.0;
     }
 
     /**
