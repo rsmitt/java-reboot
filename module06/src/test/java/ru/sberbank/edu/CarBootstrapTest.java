de0838d334c1de6910d2200761835a7c17bdaddf
Dmitry <karpov0910@mail.ru>
2023-11-21 06:21:49 +0300

    ДЗ_6 Отредактированны тесты

--- module06/src/test/java/ru/sberbank/edu/CarBootstrapTest.java
+++ module06/src/test/java/ru/sberbank/edu/CarBootstrapTest.java
@@ -1,8 +1,10 @@
 package ru.sberbank.edu;
 
+import org.h2.jdbcx.JdbcConnectionPool;
 import org.h2.tools.Server;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.testng.annotations.*;
+
 import static org.junit.Assert.*;
 
 import ru.sberbank.edu.dbconnection.H2DbEmbedded;
@@ -13,34 +15,50 @@ import ru.sberbank.edu.service.CarService;
 import ru.sberbank.edu.service.CarServiceImpl;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 
 public class CarBootstrapTest {
+    private Server server;
+    private static H2DbEmbedded h2DbEmbedded;
+
+    @BeforeClass
+    public void initDBForTests() throws SQLException {
+        String[] args = new String[]{};
+        server = Server.createTcpServer(args).start();
+        h2DbEmbedded = new H2DbEmbedded();
+        H2DbEmbedded.initDb();
+    }
+
+    @AfterEach
+    void deleteTableCar() throws Exception {
+        String readAllCarsSql = "DELETE FROM car";
+        Statement statement = H2DbEmbedded.getConnection().createStatement();
+        statement.executeQuery(readAllCarsSql);
+    }
+
+    @AfterClass
+    public void closeDB() throws Exception {
+        h2DbEmbedded.close();
+        server.stop();
+    }
+
     @Test
     public void WhenAdd() throws Exception{
-        String[] args = new String[]{};
         List<String> result = new ArrayList<String>();
         String[] expectedResult = new String[]{"777", "Lada"};
-        Server server = Server.createTcpServer(args).start();
-        H2DbEmbedded.initDb();
-        try(H2DbEmbedded h2DbEmbedded = new H2DbEmbedded()) {
-            CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
-            CarService carService = new CarServiceImpl(carRepository);
-            carService.addCar("777", "Lada");
-            String readAllCarsSql = "SELECT * FROM car";
-            Statement statement = H2DbEmbedded.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(readAllCarsSql);
-            while (resultSet.next()) {
-                result.add(resultSet.getString(1));
-                result.add(resultSet.getString(2));
-            }
+        CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
+        CarService carService = new CarServiceImpl(carRepository);
+        carService.addCar("777", "Lada");
+        String readAllCarsSql = "SELECT * FROM car";
+        Statement statement = H2DbEmbedded.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(readAllCarsSql);
+        while (resultSet.next()) {
+            result.add(resultSet.getString(1));
+            result.add(resultSet.getString(2));
         }
-        server.stop();
         assertArrayEquals(expectedResult, result.toArray());
         System.out.println(H2DbEmbedded.getConnection());
     }
@@ -48,143 +66,106 @@ public class CarBootstrapTest {
 
     @Test
     public void WhenDeleteById() throws Exception{
-        String[] args = new String[]{};
         List<String> result = new ArrayList<String>();
         String[] expectedResult = new String[]{"777", "Lada", "35", "Volvo", "55", "KIA", "65", "HONDA"};
-        Server server = Server.createTcpServer(args).start();
-        H2DbEmbedded.initDb();
-        try(H2DbEmbedded h2DbEmbedded = new H2DbEmbedded()) {
-            CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
-            CarService carService = new CarServiceImpl(carRepository);
-            carService.addCar("777", "Lada");
-            carService.addCar("35", "Volvo");
-            carService.addCar("45", "BMW");
-            carService.addCar("55", "KIA");
-            carService.addCar("65", "HONDA");
-            carService.deleteCar("45");
-            String readAllCarsSql = "SELECT * FROM car";
-            Statement statement = H2DbEmbedded.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(readAllCarsSql);
-            while (resultSet.next()) {
-                result.add(resultSet.getString(1));
-                result.add(resultSet.getString(2));
-            }
+        CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
+        CarService carService = new CarServiceImpl(carRepository);
+        carService.addCar("777", "Lada");
+        carService.addCar("35", "Volvo");
+        carService.addCar("45", "BMW");
+        carService.addCar("55", "KIA");
+        carService.addCar("65", "HONDA");
+        carService.deleteCar("45");
+        String readAllCarsSql = "SELECT * FROM car";
+        Statement statement = H2DbEmbedded.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(readAllCarsSql);
+        while (resultSet.next()) {
+            result.add(resultSet.getString(1));
+            result.add(resultSet.getString(2));
         }
-        server.stop();
         assertArrayEquals(expectedResult, result.toArray());
     }
 
     @Test
     public void WhenEditModel() throws Exception{
-        String[] args = new String[]{};
         List<String> result = new ArrayList<String>();
-        String[] expectedResult = new String[]{"777", "Lada", "35", "Volvo", "45",  "Haval", "55", "KIA", "65", "HONDA"};
-        Server server = Server.createTcpServer(args).start();
-        H2DbEmbedded.initDb();
-        try(H2DbEmbedded h2DbEmbedded = new H2DbEmbedded()) {
-            CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
-            CarService carService = new CarServiceImpl(carRepository);
-            carService.addCar("777", "Lada");
-            carService.addCar("35", "Volvo");
-            carService.addCar("45", "BMW");
-            carService.addCar("55", "KIA");
-            carService.addCar("65", "HONDA");
-            carService.editModel("45",  "Haval");
-            String readAllCarsSql = "SELECT * FROM car";
-            Statement statement = H2DbEmbedded.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(readAllCarsSql);
-            while (resultSet.next()) {
-                result.add(resultSet.getString(1));
-                result.add(resultSet.getString(2));
-            }
+        String[] expectedResult = new String[]{"777", "Lada", "35", "Volvo", "55", "KIA", "65", "HONDA", "45", "Haval"};;
+        CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
+        CarService carService = new CarServiceImpl(carRepository);
+        carService.addCar("777", "Lada");
+        carService.addCar("35", "Volvo");
+        carService.addCar("45", "BMW");
+        carService.addCar("55", "KIA");
+        carService.addCar("65", "HONDA");
+        carService.editModel("45",  "Haval");
+        String readAllCarsSql = "SELECT * FROM car";
+        Statement statement = H2DbEmbedded.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(readAllCarsSql);
+        while (resultSet.next()) {
+            result.add(resultSet.getString(1));
+            result.add(resultSet.getString(2));
         }
-        server.stop();
         assertArrayEquals(expectedResult, result.toArray());
-    }
-
-    @AfterTest
-    public void postSignUp()
-    {
-        server.stop();
     }
 
     @Test
     public void WhenCreateAll() throws Exception{
-        String[] args = new String[]{};
         List<String> result = new ArrayList();
         List<Car> forAdd = new ArrayList();
         String[] expectedResult = new String[]{"777", "Lada", "35", "Volvo","45", "BMW", "55", "KIA", "65", "HONDA"};
-        Server server = Server.createTcpServer(args).start();
-        H2DbEmbedded.initDb();
-        try(H2DbEmbedded h2DbEmbedded = new H2DbEmbedded()) {
-            CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
-            forAdd.add(new Car("777", "Lada"));
-            forAdd.add(new Car("35", "Volvo"));
-            forAdd.add(new Car("45", "BMW"));
-            forAdd.add(new Car("55", "KIA"));
-            forAdd.add(new Car("65", "HONDA"));
-            carRepository.createAll(forAdd);
-            String readAllCarsSql = "SELECT * FROM car";
-            Statement statement = H2DbEmbedded.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(readAllCarsSql);
-            while (resultSet.next()) {
-                result.add(resultSet.getString(1));
-                result.add(resultSet.getString(2));
-            }
+        CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
+        forAdd.add(new Car("777", "Lada"));
+        forAdd.add(new Car("35", "Volvo"));
+        forAdd.add(new Car("45", "BMW"));
+        forAdd.add(new Car("55", "KIA"));
+        forAdd.add(new Car("65", "HONDA"));
+        carRepository.createAll(forAdd);
+        String readAllCarsSql = "SELECT * FROM car";
+        Statement statement = H2DbEmbedded.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(readAllCarsSql);
+        while (resultSet.next()) {
+            result.add(resultSet.getString(1));
+            result.add(resultSet.getString(2));
         }
-        server.stop();
         assertArrayEquals(expectedResult, result.toArray());
     }
 
     @Test
     public void WhenDeleteAll() throws Exception{
-        String[] args = new String[]{};
         List<String> result = new ArrayList();
         List<Car> forAdd = new ArrayList();
         String[] expectedResult = new String[]{};
-        Server server = Server.createTcpServer(args).start();
-        H2DbEmbedded.initDb();
-        try(H2DbEmbedded h2DbEmbedded = new H2DbEmbedded()) {
-            CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
-            forAdd.add(new Car("777", "Lada"));
-            forAdd.add(new Car("35", "Volvo"));
-            forAdd.add(new Car("45", "BMW"));
-            forAdd.add(new Car("55", "KIA"));
-            forAdd.add(new Car("65", "HONDA"));
-            carRepository.createAll(forAdd);
-            carRepository.deleteAll();
-            String readAllCarsSql = "SELECT * FROM car";
-            Statement statement = H2DbEmbedded.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(readAllCarsSql);
-            while (resultSet.next()) {
-                result.add(resultSet.getString(1));
-                result.add(resultSet.getString(2));
-            }
+        CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
+        forAdd.add(new Car("777", "Lada"));
+        forAdd.add(new Car("35", "Volvo"));
+        forAdd.add(new Car("45", "BMW"));
+        forAdd.add(new Car("55", "KIA"));
+        forAdd.add(new Car("65", "HONDA"));
+        carRepository.createAll(forAdd);
+        carRepository.deleteAll();
+        String readAllCarsSql = "SELECT * FROM car";
+        Statement statement = H2DbEmbedded.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(readAllCarsSql);
+        while (resultSet.next()) {
+            result.add(resultSet.getString(1));
+            result.add(resultSet.getString(2));
         }
-        server.stop();
         assertArrayEquals(expectedResult, result.toArray());
     }
 
     @Test
     public void WhenFindByModel() throws Exception{
-        String[] args = new String[]{};
         Set<Car> result = new HashSet<>();
         List<Car> forAdd = new ArrayList();
         String expectedResult = "Car{id='45', model='BMW'}";
-        Server server = Server.createTcpServer(args).start();
-        H2DbEmbedded.initDb();
-        try(H2DbEmbedded h2DbEmbedded = new H2DbEmbedded()) {
-            CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
-            forAdd.add(new Car("777", "Lada"));
-            forAdd.add(new Car("35", "Volvo"));
-            forAdd.add(new Car("45", "BMW"));
-            forAdd.add(new Car("55", "KIA"));
-            forAdd.add(new Car("65", "HONDA"));
-            carRepository.createAll(forAdd);
-            result = carRepository.findByModel("BMW");
-
-        }
-        server.stop();
+        CarRepository carRepository = new CarDbRepositoryImpl(H2DbEmbedded.getConnection());
+        forAdd.add(new Car("777", "Lada"));
+        forAdd.add(new Car("35", "Volvo"));
+        forAdd.add(new Car("45", "BMW"));
+        forAdd.add(new Car("55", "KIA"));
+        forAdd.add(new Car("65", "HONDA"));
+        carRepository.createAll(forAdd);
+        result = carRepository.findByModel("BMW");
         assertEquals(expectedResult, result.toArray()[0].toString());
     }
 }
