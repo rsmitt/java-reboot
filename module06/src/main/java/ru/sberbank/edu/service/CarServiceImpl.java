bc61b6cb2c69accaf9671b6f9a8d571a01042eda
Veu Cos <veucos@gmail.com>
2023-11-30 17:14:33 +0300

    Домашнее задание №6

--- module06/src/main/java/ru/sberbank/edu/service/CarServiceImpl.java
+++ module06/src/main/java/ru/sberbank/edu/service/CarServiceImpl.java
@@ -7,18 +7,37 @@ import ru.sberbank.edu.repository.CarRepository;
 import java.sql.SQLException;
 import java.util.Optional;
 
+/**
+ * Сервис
+ */
 public class CarServiceImpl implements CarService {
-    private CarRepository carRepository;
+    private final CarRepository carRepository;
 
+    /**
+     * Констракутор
+     * @param carRepository репозиторий
+     */
     public CarServiceImpl(CarRepository carRepository) {
         this.carRepository = carRepository;
     }
 
+    /**
+     * Добавить модель машины
+     * @param id - идентификатор
+     * @param model - модель
+     * @throws SQLException особая ситуация
+     */
     @Override
     public void addCar(String id, String model) throws SQLException {
         carRepository.createOrUpdate(new Car(id, model));
     }
 
+    /**
+     * Редактировать модель
+     * @param id - идентификатор
+     * @param newModel - модель
+     * @throws SQLException особая ситуация
+     */
     @Override
     public void editModel(String id, String newModel) throws SQLException {
         Optional<Car> optCar = carRepository.findById(id);
@@ -26,6 +45,20 @@ public class CarServiceImpl implements CarService {
         updateCarModel(car, newModel);
     }
 
+    /**
+     * Удалить модель
+     * @param id - идентификатор
+     */
+    @Override
+    public void deleteCar(String id) {
+        carRepository.deleteById(id);
+    }
+
+    /**
+     * Обновление модели
+     * @param car объект машина
+     * @param newModel новая модель
+     */
     private void updateCarModel(Car car, String newModel) {
         car.setModel(newModel);
         try {
--- module06/src/test/java/ru/sberbank/edu/AppTest.java
+++ module06/src/test/java/ru/sberbank/edu/AppTest.java
@@ -1,37 +1,17 @@
 package ru.sberbank.edu;
+import org.junit.jupiter.api.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
+import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test
+    void testApp()
     {
         assertTrue( true );
     }
