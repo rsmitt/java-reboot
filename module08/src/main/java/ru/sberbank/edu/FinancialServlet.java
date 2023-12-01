8cd383c03de00f26f234915a0b600134edbf5c5c
Dmitry <karpov0910@mail.ru>
2023-12-01 07:33:25 +0300

    ДЗ_8 Задание 2

--- module08/src/main/java/ru/sberbank/edu/FinancialServlet.java
+++ module08/src/main/java/ru/sberbank/edu/FinancialServlet.java
@@ -1,13 +1,25 @@
 package ru.sberbank.edu;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
  */
-public class FinancialServlet
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@WebServlet("/HelloWorld")
+public class FinancialServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws IOException, ServletException {
+        //PrintWriter pw = response.getWriter();
+        //pw.println("<html>");
+        //pw.println("<h1> Hello World!</h1>");
+        //pw.println("</html>");
+        //request.getRequestDispatcher("/webapp/WEB-INF/index.jsp").forward(request, response);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(request, response);
     }
 }
