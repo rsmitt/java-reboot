5be4479550c1bff50c92ea43a53651a88a4b2499
kays <kaisnof@gmail.com>
2023-12-10 21:19:43 +0300

    8 дз по Java

---
+++ module08/src/main/java/ru/sberbank/edu/Deposit.java
@@ -0,0 +1,22 @@
+package ru.sberbank.edu;
+
+public class Deposit {
+
+    private double amount;
+    private double interest;
+    private double tenor;
+
+    public Deposit(double amount, double interest, double tenor) {
+        this.amount = amount;
+        this.interest = interest;
+        this.tenor = tenor;
+    }
+
+    public Deposit() {
+        this(0, 0, 0);
+    }
+
+    public double getAmountAtMaturity() {
+        return amount * Math.pow(1 + interest / 100, tenor);
+    }
+}
