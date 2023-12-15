ad27c1cf67f1973de3ee5098689c653254bf089d
RodinRoman <roman.v.rodin@gmail.com>
2023-12-15 14:04:38 +0300

    added module11

---
+++ module11/src/main/java/com/example/module11/entity/User.java
@@ -0,0 +1,8 @@
+package com.example.module11.entity;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+}
---
+++ module11/src/test/java/com/example/module11/Module11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
