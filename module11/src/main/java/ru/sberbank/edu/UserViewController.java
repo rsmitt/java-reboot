0373605e4431d1ac5d85a984ab8d0df95fe0bd22
Dmitry <karpov0910@mail.ru>
2023-12-22 07:27:30 +0300

    ДЗ_11 Добавил html страницы и hibernate зависимости

--- module11/src/main/java/ru/sberbank/edu/UserViewController.java
+++ module11/src/main/java/ru/sberbank/edu/UserViewController.java
@@ -2,9 +2,7 @@ package ru.sberbank.edu;
 
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
@@ -17,4 +15,33 @@ public class UserViewController {
         modelAndView.setViewName("user");
         return modelAndView;
     }
+
+    @GetMapping("/getUsers")
+    public ModelAndView getUsers() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("getUsers");
+        return modelAndView;
+    }
+
+    @GetMapping("/addUser")
+    public ModelAndView postUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("postUser");
+        return modelAndView;
+    }
+
+    @GetMapping("/deleteUser")
+    public ModelAndView deleteUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("deleteUser");
+        return modelAndView;
+    }
+
+    @GetMapping("/editUser")
+    public ModelAndView editUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editUser");
+        return modelAndView;
+    }
+
 }
